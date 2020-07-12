0ea4769d6a368283b02f0df80eae8cc1e51d95bf
Adam Sandberg Ericsson <adam@sandbergericsson.se>
2020-07-12 10:11:48 +0100

    cleanup

--- hie-datalog/shell.nix
+++ hie-datalog/shell.nix
@@ -4,9 +4,6 @@ pkgs.mkShell {
   buildInputs = [
     (pkgs.haskell.packages.ghc8101.ghcWithPackages (p: [
       p.ghc
-      p.extra
-      p.lens
-      p.generic-lens
     ]))
     pkgs.souffle
     pkgs.ghcid
